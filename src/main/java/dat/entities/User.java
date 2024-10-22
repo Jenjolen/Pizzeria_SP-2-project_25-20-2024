7d262b7c3c0f5ff92bc825edcd451a16c0e2c4ee
Davud05 <Drassuil@gmail.com>
2024-10-22 10:26:36 +0200

    Lavet OrderItem og user entity

--- src/main/java/dat/entities/User.java
+++ src/main/java/dat/entities/User.java
@@ -1,4 +1,29 @@
 package dat.entities;
 
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+@NoArgsConstructor
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "user_id")
+    private Integer userId;
+
+    private String name;
+    private int age;
+    private String gender;
+    private String email;
+
+    @OneToMany(mappedBy = "user")
+    private List<Order> orders = new ArrayList<>();
 }

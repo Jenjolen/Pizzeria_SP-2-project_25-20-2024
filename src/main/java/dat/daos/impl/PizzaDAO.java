6b26ed479cf820505aa0a51c57683f0a605c058d
m4cfly <nicklb@live.dk>
2024-10-22 11:17:30 +0200

    added pizza dao

---
+++ src/main/java/dat/daos/impl/PizzaDAO.java
@@ -0,0 +1,17 @@
+package dat.daos.impl;
+
+
+import dat.daos.IDAO;
+import dat.dtos.PizzaDTO;
+import dat.entities.Pizza;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.TypedQuery;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@NoArgsConstructor
+public class PizzaDAO {
+
+}

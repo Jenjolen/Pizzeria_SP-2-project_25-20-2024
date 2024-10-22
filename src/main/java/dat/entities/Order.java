c2e06ff0ed1067d91099219513073dedfe94db6c
Davud05 <Drassuil@gmail.com>
2024-10-22 10:18:42 +0200

    Lavet Order entity

--- src/main/java/dat/entities/Order.java
+++ src/main/java/dat/entities/Order.java
@@ -1,4 +1,33 @@
 package dat.entities;
 
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+@Entity
+@Table(name = "orders")
+@Getter
+@Setter
+@NoArgsConstructor
 public class Order {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "order_id")
+    private Integer orderId;
+
+    @Column(name = "order_date")
+    private LocalDateTime orderDate;
+
+    @Column(name = "order_price")
+    private double orderPrice;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @OneToMany(mappedBy = "order", cascade = CascadeType.ALL)
+    private List<OrderItem> orderItems = new ArrayList<>();
 }

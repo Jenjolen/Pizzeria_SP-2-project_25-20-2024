5fffa3fb7151b4df5c922973136846bc1d76a456
Jenjolen <cph-jn466@cphbusiness.dk>
2024-10-22 15:10:46 +0200

    Har lavet PizzaDAO, samt ændringer ift. til at inkorperere User og UserDTO i Order og OrderDTO. Vi løber evt. ind i problemer ift. Order.java, men har beskrevet de mulige problemer i en TODO besked! :)

--- src/main/java/dat/dtos/OrderDTO.java
+++ src/main/java/dat/dtos/OrderDTO.java
@@ -1,6 +1,7 @@
 package dat.dtos;
 
 import dat.entities.Order;
+import dat.security.entities.User;
 import dk.bugelhartmann.UserDTO;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -11,18 +12,21 @@ import java.util.List;
 import java.util.Set;
 import java.util.stream.Collectors;
 
-
+@Getter
+@NoArgsConstructor
 public class OrderDTO {
     private Integer id;
     private String orderDate;
     private Double orderPrice;
     private Order.PizzaType pizzaType;
     private Set<PizzaDTO> pizzas = new HashSet<>();
+    private UserDTO user;
 
     public OrderDTO(Order order){
         this.id = order.getId();
         this.orderDate = order.getOrderDate();
         this.orderPrice = order.getOrderPrice();
+        this.user = new UserDTO(order.getUser().getUsername(), order.getUser().getRoles().stream().map(r -> r.getRoleName()).collect(Collectors.toSet()));
         if (order.getOrderLists() != null)
         {
             order.getOrderLists().forEach( pizza -> pizzas.add(new PizzaDTO(pizza.getPizza())));

af5e39ddb900e5590e22cfd527f7de87cbdbea24
Davud05 <Drassuil@gmail.com>
2024-10-22 10:06:42 +0200

    Lavet pizza entity

--- src/main/java/dat/entities/Pizza.java
+++ src/main/java/dat/entities/Pizza.java
@@ -1,4 +1,29 @@
 package dat.entities;
 
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "pizza")
+@Getter
+@Setter
+@NoArgsConstructor
 public class Pizza {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "pizza_id")
+    private Integer pizzaId;
+
+    private String name;
+    private String description;
+    private String toppings;
+    private double price;
+
+    @OneToMany(mappedBy = "pizza")
+    private List<OrderItem> orderItems = new ArrayList<>();
 }
